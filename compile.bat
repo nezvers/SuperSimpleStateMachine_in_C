dbd47a22f4a4674f64a2c7366883ab7af683215f
Agnis "NeZvērs" Aldiņš <agnis16@inbox.lv>
2021-01-19 11:15:54 +0200

    Update compile.bat

--- compile.bat
+++ compile.bat
@@ -1,4 +1,21 @@
-del main.exe
-gcc main.c -o main.exe
-main.exe
+@echo off
+::=======================================
+:: Set compilation variables
+set cPath=
+set cc=gcc
+set source=main.c
+set libs=
+set output=main.exe
+set cFlags=-std=c99 -Wall
+::=======================================
+:: Remove previous compilation
+cmd /c IF EXIST %output% del /F %output%
+::=======================================
+::Compile
+%cPath%%cc% %source% -o %output% %cFlags% %libs%
+::=======================================
+::launch the executable
+%output%
+::=======================================
+::stop at the end 
 pause
